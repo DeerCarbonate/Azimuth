3d57c5236f66bb1f00611476e4af637b86d7d06a
DeerCarbonate <155916106+DeerCarbonate@users.noreply.github.com>
2026-05-02 09:27:23 +0500

    0.5 update

--- scripts/stationAbility.js
+++ scripts/stationAbility.js
@@ -1,12 +1,15 @@
 const UNIT_NAME = "Legate";
-
 Core.app.addListener({
     update() {
         if (!Vars.state.isGame()) return;
         Groups.unit.each(unit => {
             if (unit.type.name !== UNIT_NAME) return;
+            print("Found Legate unit!");
+            print("Unit disarmed before: " + unit.disarmed);
+            print("Unit has volatile: " + unit.hasEffect(StatusEffects.volatile));
             unit.disarmed = false;
             unit.apply(StatusEffects.volatile, 2);
+            print("Unit disarmed after: " + unit.disarmed);
         });
     }
 });
