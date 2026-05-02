f3b30047cfb7cbc59f0cb68660dc02a0a21a13b4
DeerCarbonate <155916106+DeerCarbonate@users.noreply.github.com>
2026-05-02 09:20:52 +0500

    0.5 update

--- scripts/stationAbility.js
+++ scripts/stationAbility.js
@@ -1,16 +1,12 @@
-const StatusEffects = Packages.mindustry.content.StatusEffects;
-const Groups     = Packages.mindustry.Groups;
-const Core       = Packages.arc.Core;
-const Vars       = Packages.mindustry.Vars;
 const UNIT_NAME = "Legate";
+
 Core.app.addListener({
     update() {
         if (!Vars.state.isGame()) return;
-
         Groups.unit.each(unit => {
             if (unit.type.name !== UNIT_NAME) return;
             unit.disarmed = false;
-            unit.apply(StatusEffects.volatile, 2); 
+            unit.apply(StatusEffects.volatile, 2);
         });
     }
 });
