7303ef520b5ba851f1b22272267a01a51f116f9d
DeerCarbonate <155916106+DeerCarbonate@users.noreply.github.com>
2026-05-29 09:46:57 +0500

    0.5 update

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,29 @@ const mod = Vars.mods.locateMod("azimut");
 mod.meta.displayName =
 "[#b867ec]A[#c974dc]Z[#bb9ef3ff]I[#9a5bf6]M[#934bee]U[#8b75e1]T[#c1abd4]H";
 
+mod.meta.description =
+"A [#c5faf6]hybrid mod[] primarily built around [#bbc4f4]Erekir[] and its linear progression system.\n" +
+
+"The mod introduces new gameplay mechanics that are tightly integrated into the campaign and accompany you throughout the entire playthrough.\n\n" +
+
+"The mod also has a [#768bff]Discord server[] — you can find the link in the README file on the GitHub page.\n\n" +
+
+"[red]THIS MOD IS STILL IN DEVELOPMENT![]\n" +
+"At the moment, development is approximately [accent]52%[] complete and features [accent]12 fully polished sectors[].\n\n" +
+
+"////////////////////////////////\n\n" +
+
+"Land on a [#c9c1eb]subcryogenic terra[], gradually warming under its star, known as [#768bff]Gelion[].\n" +
+"Your objective is to eliminate enemy forces on the planet, secure rare resources, and continue your expansion across the galaxy.\n\n" +
+
+"Limited space and complex logistics present a serious challenge.\n" +
+"[#fac5d3]Plan your production wisely![]\n\n" +
+
+"If you enjoy the mod, consider giving it a star! >~<\n" +
+"Thanks for playing!\n\n" +
+
+"Special thanks to: [#bf92f9]ace1020spawn[white], [#ce7e5c]Kivet[#9b5e45]/БОБ[white], [#84f490]Alpotat[white], KO[#A4A4A4]STO[#6D6D6D]LOM [white], [#c1abd4]Fate of Adrastus[white], [#f7ba4c]garretski[white]";
+
 mod.meta.author = "[#c3961f]Deer[#876409]Carbonate";
 
 require("blocks");
